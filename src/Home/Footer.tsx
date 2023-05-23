33cacbef9e105fdfcdd72d6138f5d1a51d703be2
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-05-23 17:42:55 +0300

    updata the footer

--- src/Home/Footer.tsx
+++ src/Home/Footer.tsx
@@ -66,6 +66,7 @@
 
 // export default Footer;
 
+
 import {Link} from "react-router-dom";
 
 const Footer = () => {
@@ -74,7 +75,7 @@ const Footer = () => {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
-        className="absolute bottom-0 left-0 right-0"
+        className="absolute bottom-0 left-0 right-0 "
       >
         <path
           fill="#fff"
@@ -118,24 +119,7 @@ const Footer = () => {
         </div>
         <hr className="border-green-50 mb-8" />
 
-        <div className="flex justify-center mt-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            className="h-12 w-12 text-green-100"
-          >
-            <path
-              fill="currentColor"
-              d="M372 64c0-35.346-28.654-64-64-64H88C52.654 0 24 28.654 24 64c0 6.627 5.373 12 12 12s12-5.373 12-12c0-23.196 18.804-42 42-42h220c23.196 0 42 18.804 42 42 0 6.627 5.373 12 12 12s12-5.373 12-12zm116 80H348c-5.523 0-10 4.477-10 10v180c0 39.701-32.299 72-72 72h-84c-39.701 0-72-32.299-72-72V154c0-5.523-4.477-10-10-10H24c-13.255 0-24 10.745-24 24v188c0 39.701 32.299 72 72 72h184c8.822 0 16 7.178 16 16s-7.178 16-16 16H72c-57.897 0-105-47.103-105-105V138c0-57.897 47.103-105 105-105h404c57.897 0 105 47.103 105 105v188c0 57.897-47.103 105-105 105z"
-            />
-          </svg>
-          <span className="text-gray-800 ml-2">
-            Explore the diverse flavors from around the world. Indulge in our
-            delicious recipes and discover new culinary experiences. Bon
-            appétit!
-          </span>
-        </div>
-        <div className="flex justify-center mt-8">
+        <div className="flex justify-center">
           <span className="text-gray-800">
             © 2023{" "}
             <Link to="/" className="hover:underline text-blue-700">
